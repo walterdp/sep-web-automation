7187198aa049ba7aaa9ec73d4513dc4bd2f61a72
walterdp <walterdiazp30@gmail.com>
2026-03-06 12:29:32 -0600

    Ejemplo de demo

--- tests/pages/cotizador.page.js
+++ tests/pages/cotizador.page.js
@@ -56,6 +56,10 @@ export class CotizadorPage {
     await this.montoInput.fill((value));
   }
 
+  async getMontoValue() {
+    return await this.montoInput.inputValue();
+  }
+
    async takeScreenshot(route) {
     await this.page.screenshot({ path: route, fullPage: true });
   }
